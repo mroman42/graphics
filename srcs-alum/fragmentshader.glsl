daa25450b02ff6c7aba782f98d21af7ffd3a1102
M42 <mromang08@gmail.com>
2017-10-03 13:17:36 +0200

    glsl

---
+++ srcs-alum/fragmentshader.glsl
@@ -0,0 +1,8 @@
+void main() {
+  // El objetivo es escribir la variable:
+  //  - gl_FragColor, color a asignar al pixel
+  //
+  // Puede leerse la variable:
+  //  - gl_Color, color de la primitiva
+  gl_FragColor = gl_Color;
+}
---
+++ srcs-alum/vertexshader.glsl
@@ -0,0 +1,13 @@
+void main() {
+  // El objetivo es escribir estas variables:
+  //  - gl_Position, posición del vértice en pantalla,
+  //  - gl_FrontColor, color asociado al vértice.
+  //
+  // Se pueden leer (entre otras) las variables:
+  //  - gl_ModelViewProjection, matriz actual de transformación
+  //  - gl_Vertex, coordenadas del punto
+  //  - gl_Color, color actual especificado
+
+  gl_FrontColor = gl_Color;
+  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}
